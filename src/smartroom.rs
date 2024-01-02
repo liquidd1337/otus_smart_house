0b87bd55064259b7279c9b9b33b18c213e847ec1
liquidd1337 <mikle-1910@mail.ru>
2024-01-02 17:53:27 +0700

    add thiserror in smartroom crate

--- src/smartroom.rs
+++ src/smartroom.rs
@@ -1,6 +1,15 @@
-use crate::devices::*;
+use thiserror::Error;
 
+use crate::devices::*;
 use std::{collections::HashMap, fmt::Display};
+
+#[derive(Debug, Error)]
+pub enum SmartRoomError {
+    #[error("Error while adding device: {0}")]
+    AddDeviceError(#[from] DeviceError),
+    #[error("Error while deleting device")]
+    DeleteDeviceError(String),
+}
 #[derive(Debug, Clone)]
 pub struct SmartRoom {
     pub room_name: String,
@@ -15,15 +24,21 @@ impl SmartRoom {
         }
     }
 
-    pub fn add_smart_device(&mut self, smart_device: Device)  {
-        if let Some(device_name) = smart_device.device_name() {
+    pub fn add_smart_device(&mut self, smart_device: Device) -> Result<(), SmartRoomError> {
+        if let Ok(device_name) = smart_device.device_name() {
             self.smart_device.insert(device_name, smart_device);
+            Ok(())
+        } else {
+            Err(SmartRoomError::AddDeviceError(smart_device.device_name().unwrap_err()))
         }
     }
 
-    pub fn delite_device(&mut self, smart_device: Device) {
-        if let Some(device_name) = smart_device.device_name() {
+    pub fn delite_device(&mut self, smart_device: Device) -> Result<(), SmartRoomError> {
+        if let Ok(device_name) = smart_device.device_name() {
             self.smart_device.remove(&device_name);
+            Ok(())
+        } else {
+            Err(SmartRoomError::DeleteDeviceError("The device does not exist".to_string()))
         }
     }
 }
@@ -48,6 +63,7 @@ mod tests {
     fn add_smart_device() {
         let mut smart_room = SmartRoom::default("kitchen".to_string());
         let soket = Device::SmartSocket(SmartSocket::default("soket".to_string()));
+        #[allow(dead_code)]
         smart_room.add_smart_device(soket);
         assert!(!smart_room.smart_device.is_empty());
     }
@@ -56,6 +72,7 @@ mod tests {
     fn delite_device() {
         let mut smart_room = SmartRoom::default("kitchen".to_string());
         let soket = Device::SmartSocket(SmartSocket::default("socket".to_string()));
+        #[allow(dead_code)]
         smart_room.add_smart_device(soket.clone());
         assert!(!smart_room.smart_device.is_empty());
         smart_room.delite_device(soket);
