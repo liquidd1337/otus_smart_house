158d7d42c517b0b364238d9f0dba9351c30c680f
liquidd1337 <mikle-1910@mail.ru>
2024-01-02 20:34:15 +0700

    add unwrap in main

--- examples/main.rs
+++ examples/main.rs
@@ -11,21 +11,21 @@ fn main() {
     //Инициализация комнат
 
     let mut kitchen = SmartRoom::default("Kitchen".to_string());
-    kitchen.add_smart_device(Device::SmartSocket(socket.clone()));
+    kitchen.add_smart_device(Device::SmartSocket(socket.clone())).unwrap();
     let mut hall = SmartRoom::default("Hall".to_string());
-    hall.add_smart_device(Device::SmartSocket(socket.clone()));
+    hall.add_smart_device(Device::SmartSocket(socket.clone())).unwrap();
     let mut bathroom = SmartRoom::default("Bathroom".to_string());
-    bathroom.add_smart_device(Device::SmartThermometr(thermo.clone()));
+    bathroom.add_smart_device(Device::SmartThermometr(thermo.clone())).unwrap();
     let mut living = SmartRoom::default("Living room".to_string());
-    living.add_smart_device(Device::SmartThermometr(thermo.clone()));
-    living.add_smart_device(Device::SmartThermometr(thermo.clone()));
+    living.add_smart_device(Device::SmartThermometr(thermo.clone())).unwrap();
+    living.add_smart_device(Device::SmartThermometr(thermo.clone())).unwrap();
     // Инициализация дома
 
     let mut house = SmartHouse::new("House".to_string());
-    house.add_smart_room(kitchen);
-    house.add_smart_room(bathroom);
-    house.add_smart_room(living);
-    house.add_smart_room(hall.clone());
+    house.add_smart_room(kitchen).unwrap();
+    house.add_smart_room(bathroom).unwrap();
+    house.add_smart_room(living).unwrap();
+    house.add_smart_room(hall.clone()).unwrap();
 
     // Строим отчёт с использованием `OwningDeviceInfoProvider`.
     let info_provider_1 = OwningDeviceInfoProvider {socket};
@@ -33,7 +33,7 @@ fn main() {
     let report1 = house.create_report(info_provider_1);
 
     // Строим отчёт с использованием `BorrowingDeviceInfoProvider`.
-    house.remove_smart_room(hall);
+    house.remove_smart_room(hall).unwrap();
     let info_provider_2 = BorrowingDeviceInfoProvider {
         socket: &socket_borrow,
         thermo: &thermo,
