a59d7015ca7dc8a8be0620095961f7bf9ea4b57e
liquidd1337 <mikle-1910@mail.ru>
2024-01-02 17:30:24 +0700

    Add crate thiserror in devices

--- src/devices.rs
+++ src/devices.rs
@@ -1,21 +1,37 @@
 use std::fmt::Display;
-#[derive(Debug, Clone)]
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum DeviceError {
+    #[error("The device doesn't have a name")]
+    DeviceNotName(String),
+}
+
+#[derive(Debug, Clone, Error)]
 pub enum Device {
     SmartSocket(SmartSocket),
     SmartThermometr(SmartThermometer),
 }
 
 impl Device {
-    pub fn device_name(&self) -> Option<String> {
+    pub fn device_name(&self) -> Result<String, DeviceError>  {
         match self {
-            Device::SmartSocket(smart_socket) => Some(smart_socket.name.clone()),
-            Device::SmartThermometr(smart_thermometer) => Some(smart_thermometer.name.clone()),
+            Device::SmartSocket(smart_socket) => Ok(smart_socket.name.clone()),
+            Device::SmartThermometr(smart_thermometer) => Ok(smart_thermometer.name.clone()),
+        }
+    }
+}
+impl std::fmt::Display for Device {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Device::SmartSocket(smart_socket) => write!(f, "{}", smart_socket),
+            Device::SmartThermometr(smart_thermometer) => write!(f, "{}", smart_thermometer),
         }
     }
 }
 
 #[allow(dead_code)]
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Error)]
 pub struct SmartSocket {
     pub name: String,
     status: bool,
@@ -41,7 +57,7 @@ impl Display for SmartSocket {
     }
 }
 #[allow(dead_code)]
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Error)]
 pub struct SmartThermometer {
     pub name: String,
     status: bool,
@@ -76,13 +92,13 @@ mod tests {
     #[test]
     fn default_socket() {
         let socket = Device::SmartSocket(SmartSocket::default("Smart Socket".to_string()));
-        assert!(socket.device_name().is_some());
+        assert!(socket.device_name().is_ok());
     }
 
     #[test]
     fn default_thermo() {
         let thermo =
             Device::SmartThermometr(SmartThermometer::default("Smart thermometr".to_string()));
-        assert!(thermo.device_name().is_some());
+        assert!(thermo.device_name().is_ok());
     }
 }
--- src/smartroom.rs
+++ src/smartroom.rs
@@ -1,4 +1,5 @@
 use crate::devices::*;
+
 use std::{collections::HashMap, fmt::Display};
 #[derive(Debug, Clone)]
 pub struct SmartRoom {
